04b41016339a6986b9c3fee3e7df27c23e4cfa56
Carlos <carlosborjawebdev@gmail.com>
2025-04-03 10:38:51 +0200

    Initial structure commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 1- Login
+// 2- Gen Api Key
+// 3- Request API
+// 4- Make queries reach the backend
+// 5- Responses
+
+console.log("Database URL:", process.env.DATABASE_URL);
+
+app.get("/", (req, res) => {
+  res.send(`Server running on port ${PORT}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
